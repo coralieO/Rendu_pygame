fb2c4cc4ac332e470b3a71745c62f3ca50a568c3
loacoco <66422809+loacoco@users.noreply.github.com>
2022-01-04 18:52:00 +0100

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# Rendu_pygame" 
+
+#Hangman game
+
+##You must import math and random to run the program.
---
+++ hangman.py
@@ -0,0 +1,141 @@
+import pygame
+import math
+import random
+
+# setup desplay
+pygame.init()
+WIDTH, HEIGHT = 1000, 800
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Hangman!")
+
+# button
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 700
+A = 65
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x, y, chr(A + i), True])
+
+# font
+LETTER_FONT = pygame.font.SysFont('comicsans', 40)
+WORD_FONT = pygame.font.SysFont('comicsans', 60)
+
+
+# Images
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman" + str(i) + ".png")
+    images.append(image)
+
+# variables
+hangman_status = 0
+
+
+guessed = []
+
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def recover_list_words():
+    words_list=[]
+    """Retourne une liste de mots récupéré à partir d'un fichier externe"""
+    with open("Mots_pendu.txt") as fichier_lecture:
+        for ligne in fichier_lecture:
+            words_list.append(ligne.rstrip("\n"))
+    return words_list
+
+words = recover_list_words()
+print(words)
+word = random.choice(words)
+
+
+
+def draw():
+    win.fill(WHITE)
+
+
+
+    # draw word
+    display_word = ""
+    for letter in word:
+        if letter in guessed:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    text = WORD_FONT.render(display_word, 1, BLACK)
+    win.blit(text, (130, 35))
+
+    # draw buttons
+    for letter in letters:
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            win.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
+
+    win.blit(images[hangman_status], (200, 150))
+    pygame.display.update()
+
+
+def display_message(message):
+    pygame.time.delay(1000)
+    win.fill(WHITE)
+    text = WORD_FONT.render(message, 1, BLACK)
+    win.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
+
+def main():
+    global hangman_status
+
+    FPS = 60
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                m_x, m_y = pygame.mouse.get_pos()
+                for letter in letters:
+                    x, y, ltr, visible = letter
+                    if visible:
+                        dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
+                        if dis < RADIUS:
+                            letter[3] = False
+                            guessed.append(ltr)
+                            if ltr not in word:
+                                hangman_status += 1
+
+        draw()
+
+        won = True
+        for letter in word:
+            if letter not in guessed:
+                won = False
+                break
+
+        if won:
+            display_message(f"You WON!{word} ")
+            display_message( "WAS THE RIGHT WORD")
+
+            break
+
+        if hangman_status == 6:
+            display_message(f"You LOST! THE WORD WAS {word}")
+            break
+
+
+while True:
+    main()
+pygame.quit()
